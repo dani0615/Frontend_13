0937fd7e82894c090fc2816325648a76e87b7eec
haluskad <haluskad@201-14>
2025-11-24 07:33:44 +0100

    mellekhatas gyakorlas , pizza hf feladat

---
+++ feladatok/mellekhatas/src/App.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const ListaKomponens = ({ elemek }) => (
+  <ol>
+    {Object.values(elemek).map((elem, index) => (
+      <li key={index}>{elem.name} - {elem.email}</li>
+    ))}
+  </ol>
+);
+export const App = () => {
+  const [adatok, setAdatok] = useState({});
+
+  useEffect(() => {
+    fetch('https://itmp.sulla.hu/users')
+    .then((res) => (res.ok? res.json() : []))
+    .then((tartalom) => setAdatok(tartalom));
+  }, [])
+
+  return (
+    <div className="container">
+      <div className="row m-5 p-5 border">
+        <ListaKomponens elemek={adatok} />
+      </div>
+    </div>
+  )
+}
+
+export default App;
